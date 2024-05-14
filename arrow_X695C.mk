97aaeccc69ba431ebfdf741c1ec94ce1f9e34cff
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2024-05-14 19:44:49 +0700

    😠😠😠😠😠

--- arrow_X695C.mk
+++ arrow_X695C.mk
@@ -14,8 +14,24 @@ $(call inherit-product, vendor/arrow/config/common.mk)
 # Inherit from Infinix-X695C device
 $(call inherit-product, device/infinix/X695C/device.mk)
 
+# Inherit some common ArrowOS stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+DEVICE_MAINTAINER := Miraclev1
+
 PRODUCT_DEVICE := X695C
 PRODUCT_NAME := arrow_X695C
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X695C
 PRODUCT_MANUFACTURER := infinix
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+TARGET_VENDOR := infinix
+TARGET_VENDOR_PRODUCT_NAME := X695C
+
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_x695_h854-user 11 RP1A.200720.011 156472 release-keys"
+
+BUILD_FINGERPRINT := Infinix/X695C-GL/Infinix-X695C:11/RP1A.200720.011/221114V356:user/release-keys
