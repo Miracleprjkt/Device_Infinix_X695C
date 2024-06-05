6b0f7e54164d088224f69b3a8f6102a7f15f631d
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2024-06-05 13:40:48 +0700

    X695C: add makefiles including x695c

--- Android.mk
+++ Android.mk
@@ -3,9 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter X695C, $(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),X695C)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
